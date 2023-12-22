97c6041a8828b036899c21128a03319fd0ac7314
Pasha Mayba <pashamayba7@gmail.com>
2023-12-22 13:37:25 +0200

    Added data validation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import HomePage from './components/home/HomePage'
-import NoMatchPage from './components/404/NoMatchPage'
-import MainMenu from './components/menu/MainMenu'
-import CreateCategory from './components/Category/AddCategoryForm'
-
-
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './components/home/HomePage';
+import NoMatchPage from './components/404/NoMatchPage';
+import MainMenu from './components/menu/MainMenu';
+import CreateCategory from './components/Category/AddCategoryForm';
+import EditCategory from './components/Category/EditCategoryPage';
 
 const App = () => {
   return (
     <>
-     <MainMenu />
-     <Routes>
-      <Route path="/">
-        <Route index element={<HomePage/>}/>
+      <MainMenu />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/create-category" element={<CreateCategory />} />
-        <Route path="*" element={<NoMatchPage/>}/>
-      </Route>
-     </Routes>
+        <Route path="/edit-category/:categoryId" element={<EditCategory />} />
+        <Route path="*" element={<NoMatchPage />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Table } from 'antd';
+import { Table, Popconfirm, Button } from 'antd';
 import { ColumnsType } from "antd/es/table";
+import { useNavigate } from "react-router-dom";
 
 interface ICategoryItem {
   id: number;
@@ -10,20 +11,36 @@ interface ICategoryItem {
 }
 
 const HomePage = () => {
+  const navigate = useNavigate();
   const [list, setList] = useState<ICategoryItem[]>([]);
 
   useEffect(() => {
-    axios
-      .get<ICategoryItem[]>("http://rozetka.com/api/categories")
-      .then((resp) => {
-        console.log("Resp data", resp.data);
-        // const dataWithImageUrls = resp.data.map(item => ({
-        //   ...item,
-        //   imageUrl: `http://rozetka.com/upload/150_${item.image}` 
-        // }));
-        setList(resp.data);
-      });
+    fetchCategories();
   }, []);
+
+  const fetchCategories = () => {
+    axios.get<ICategoryItem[]>("http://rozetka.com/api/categories")
+      .then((resp) => {
+        setList(resp.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching categories:', error);
+      });
+  };
+
+  const handleDelete = (id: number) => {
+    axios.delete(`http://rozetka.com/api/categories/${id}`)
+      .then(() => {
+        fetchCategories();
+      })
+      .catch((error) => {
+        alert(`There was an error deleting the category: ${error.message}`);
+      });
+  };
+
+  const goToEditPage = (categoryId: number) => {
+    navigate(`/edit-category/${categoryId}`); // Припустимо, що шлях для редагування категорії це /edit-category/:id
+  };
 
   const columns: ColumnsType<ICategoryItem> = [
     {
@@ -40,9 +57,30 @@ const HomePage = () => {
       title: 'Image',
       dataIndex: 'image',
       key: 'image',
-      render: (image: string) => { return (
-        <img src={`http://rozetka.com/upload/150_${image}`} alt={"Image"}/>)}
-    
+      render: (image: string) => (
+        <img src={`http://rozetka.com/upload/150_${image}`} alt={"Image"} style={{ width: '50px' }}/>
+      )
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record: ICategoryItem) => (
+        <>
+          <Button onClick={() => goToEditPage(record.id)} type="primary">
+            Edit
+          </Button>
+          <Popconfirm
+            title="Are you sure you want to delete this category?"
+            onConfirm={() => handleDelete(record.id)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button style={{ marginLeft: 8 }} danger>
+              Delete
+            </Button>
+          </Popconfirm>
+        </>
+      ),
     },
   ];
 
